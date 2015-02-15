659075b2427d240ada9bb5c0d3530b2a9964dd68
mbp2011 <webmaster@mbp2011.org>
2015-02-15 01:52:49 -0500

    code cleanup

--- config.php
+++ config.php
@@ -4,36 +4,39 @@
 date_default_timezone_set('EST');                   // set default timezone
 
 //BASE URL
-define( "BASE_URL", baseUrl() );                    // set database host
+define( 'BASE_URL', baseUrl() );                    // set database host
 
 //DOMAIN
-define( "BASE_DOMAIN", getDomain() );               // set database domain
+define( 'BASE_DOMAIN', getDomain() );               // set database domain
 
 //DATABASE
-define( "DB_HOST", "localhost" );                   // set database host
-define( "DB_USER", "root" );                        // set database user
-define( "DB_PASS", "" );                            // set database password
-define( "DB_NAME", "main" );                        // set database name
-define( "SEND_ERRORS_TO", "" );                     // set email notification address
+define( 'DB_HOST', 'localhost' );                   // set database host
+define( 'DB_USER', 'root' );                        // set database user
+define( 'DB_PASS', '' );                            // set database password
+define( 'DB_NAME', 'main' );                        // set database name
+define( 'SEND_ERRORS_TO', '' );                     // set email notification address
 
 //DEBUGGING
-define( "DISPLAY_DEBUG", false );                    // display errors?
+define( 'DISPLAY_DEBUG', false );                   // display errors?
 
 //CUSTOMIZE
 define ('SITE_NAME', '#MBP2011');                  								  // set the name of the site
-define ('SITE_SLOGAN', '2011 MacBook Pro, GPU Failure, Image Distortion');                  // set the name of the site
-define ('SITE_EMAIL', 'webmaster@' . BASE_DOMAIN ); 								  // set the site email address
+define ('SITE_SLOGAN', '2011 MacBook Pro, GPU Failure, Image Distortion');        // set the name of the site
+define ('SITE_EMAIL', 'webmaster@' . BASE_DOMAIN ); 						      // set the site email address
 define ('SITE_ADDRESS1', '');       											  // set site address
-define ('SITE_ADDRESS2', '');                       								  // set site address
-define ('SITE_CITY', '');                       									  // set site address
+define ('SITE_ADDRESS2', '');                       							  // set site address
+define ('SITE_CITY', '');                       								  // set site address
 define ('SITE_STATE', '');                     									  // set site address
 define ('SITE_COUNTRY', '');                 							   	   	  // set site address
 define ('SITE_ZIP', '');                       									  // site zip
 define ('SITE_PHONE', '');              										  // site phone number
 
-define('RSS_URL', 'http://feeds.feedburner.com/2011MacbookPro-GoogleNews');			  //RSS Url
+//PAGE-SPECIFIC
+define('RSS_URL', 'http://feeds.feedburner.com/2011MacbookPro-GoogleNews');		  //RSS Url
 
 //AWS access info
-define( "AWS_BUCKET", 'mbp2011' );                 // Upload to this bucket
-define( "AWS_ACCESS_KEY", '' );                    // AWS S3 Access Key
-define( "AWS_SECRET_KEY", '' );                    // AWS S3 Secret Key
+define( 'AWS_BUCKET', 'mbp2011' );                  // Upload to this bucket
+define( 'AWS_ACCESS_KEY', '' );                     // AWS S3 Access Key
+define( 'AWS_SECRET_KEY', '' );                     // AWS S3 Secret Key
+define('THUMBNAIL_IMAGE_MAX_WIDTH', 320);           // AWS Thumbnail Size
+define('THUMBNAIL_IMAGE_MAX_HEIGHT', 640);          // AWS Thumbnail Size
--- lib/class.library.php
+++ lib/class.library.php
@@ -92,7 +92,8 @@ function getIP()
     }
 }
 
-function remoteFilesize($url) {
+function remoteFilesize($url)
+{
     static $regex = '/^Content-Length: *+\K\d++$/im';
     if (!$fp = @fopen($url, 'rb')) {
         return false;
@@ -106,7 +107,8 @@ function remoteFilesize($url) {
     return strlen(stream_get_contents($fp));
 }
 
-function humanFilesize($bytes, $decimals = 2) {
+function humanFilesize($bytes, $decimals = 2)
+{
   $sz = 'BKMGTP';
   $factor = floor((strlen($bytes) - 1) / 3);
   return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
@@ -114,14 +116,14 @@ function humanFilesize($bytes, $decimals = 2) {
 
 function calculateMD5($file)
 {
-	if (remoteFilesize($file) < 5120 ) 
+	if (remoteFilesize($file) < 5120 )
 	{
 		$checksum = md5_file($file);
 
-		return $checksum; 
+		return $checksum;
 	}
-	
-	return false;	
+
+	return false;
 }
 
 /* INPUT FUNCTIONS */
@@ -155,7 +157,7 @@ function checkEmail($email)
 
 
 /* OUTPUT FUNCTIONS */
-function html2text($Document) 
+function html2text($Document)
 {
     $Rules = array ('@<script[^>]*?>.*?</script>@si',
                     '@<[\/\!]*?[^<>]*?>@si',
@@ -190,22 +192,23 @@ function html2text($Document)
   return preg_replace($Rules, $Replace, $Document);
 }
 
-function extractCommonWords($string){
+function extractCommonWords($string)
+{
       $stopWords = array("onepagecms","class","a","able","about","above","abroad","according","accordingly","across","actually","adj","after","afterwards","again","against","ago","ahead","ain't","all","allow","allows","almost","alone","along","alongside","already","also","although","always","am","amid","amidst","among","amongst","an","and","another","any","anybody","anyhow","anyone","anything","anyway","anyways","anywhere","apart","appear","appreciate","appropriate","are","aren't","around","as","a's","aside","ask","asking","associated","at","available","away","awfully","b","back","backward","backwards","be","became","because","become","becomes","becoming","been","before","beforehand","begin","behind","being","believe","below","beside","besides","best","better","between","beyond","both","brief","but","by","c","came","can","cannot","cant","can't","caption","cause","causes","certain","certainly","changes","clearly","c'mon","co","co.","com","come","comes","concerning","consequently","consider","considering","contain","containing","contains","corresponding","could","couldn't","course","c's","currently","d","dare","daren't","definitely","described","despite","did","didn't","different","directly","do","does","doesn't","doing","done","don't","down","downwards","during","e","each","edu","eg","eight","eighty","either","else","elsewhere","end","ending","enough","entirely","especially","et","etc","even","ever","evermore","every","everybody","everyone","everything","everywhere","ex","exactly","example","except","f","fairly","far","farther","few","fewer","fifth","first","five","followed","following","follows","for","forever","former","formerly","forth","forward","found","four","from","further","furthermore","g","get","gets","getting","given","gives","go","goes","going","gone","got","gotten","greetings","h","had","hadn't","half","happens","hardly","has","hasn't","have","haven't","having","he","he'd","he'll","hello","help","hence","her","here","hereafter","hereby","herein","here's","hereupon","hers","herself","he's","hi","him","himself","his","hither","hopefully","how","howbeit","however","hundred","i","i'd","ie","if","ignored","i'll","i'm","immediate","in","inasmuch","inc","inc.","indeed","indicate","indicated","indicates","inner","inside","insofar","instead","into","inward","is","isn't","it","it'd","it'll","its","it's","itself","i've","j","just","k","keep","keeps","kept","know","known","knows","l","last","lately","later","latter","latterly","least","less","lest","let","let's","like","liked","likely","likewise","little","look","looking","looks","low","lower","ltd","m","made","mainly","make","makes","many","may","maybe","mayn't","me","mean","meantime","meanwhile","merely","might","mightn't","mine","minus","miss","more","moreover","most","mostly","mr","mrs","much","must","mustn't","my","myself","n","name","namely","nd","near","nearly","necessary","need","needn't","needs","neither","never","neverf","neverless","nevertheless","new","next","nine","ninety","no","nobody","non","none","nonetheless","noone","no-one","nor","normally","not","nothing","notwithstanding","novel","now","nowhere","o","obviously","of","off","often","oh","ok","okay","old","on","once","one","ones","one's","only","onto","opposite","or","other","others","otherwise","ought","oughtn't","our","ours","ourselves","out","outside","over","overall","own","p","particular","particularly","past","per","perhaps","placed","please","plus","possible","presumably","probably","provided","provides","q","que","quite","qv","r","rather","rd","re","really","reasonably","recent","recently","regarding","regardless","regards","relatively","respectively","right","round","s","said","same","saw","say","saying","says","second","secondly","see","seeing","seem","seemed","seeming","seems","seen","self","selves","sensible","sent","serious","seriously","seven","several","shall","shan't","she","she'd","she'll","she's","should","shouldn't","since","six","so","some","somebody","someday","somehow","someone","something","sometime","sometimes","somewhat","somewhere","soon","sorry","specified","specify","specifying","still","sub","such","sup","sure","t","take","taken","taking","tell","tends","th","than","thank","thanks","thanx","that","that'll","thats","that's","that've","the","their","theirs","them","themselves","then","thence","there","thereafter","thereby","there'd","therefore","therein","there'll","there're","theres","there's","thereupon","there've","these","they","they'd","they'll","they're","they've","thing","things","think","third","thirty","this","thorough","thoroughly","those","though","three","through","throughout","thru","thus","till","to","together","too","took","toward","towards","tried","tries","truly","try","trying","t's","twice","two","u","un","under","underneath","undoing","unfortunately","unless","unlike","unlikely","until","unto","up","upon","upwards","us","use","used","useful","uses","using","usually","v","value","various","versus","very","via","viz","vs","w","want","wants","was","wasn't","way","we","we'd","welcome","well","we'll","went","were","we're","weren't","we've","what","whatever","what'll","what's","what've","when","whence","whenever","where","whereafter","whereas","whereby","wherein","where's","whereupon","wherever","whether","which","whichever","while","whilst","whither","who","who'd","whoever","whole","who'll","whom","whomever","who's","whose","why","will","willing","wish","with","within","without","wonder","won't","would","wouldn't","x","y","yes","yet","you","you'd","you'll","your","you're","yours","yourself","yourselves","you've","z","zero",'www');
-   
+
       $string = preg_replace('/\s\s+/i', '', $string); // replace whitespace
       $string = trim($string); // trim the string
       $string = preg_replace('/[^a-zA-Z0-9 -]/', '', $string); // only take alphanumerical characters, but keep the spaces and dashes too…
       $string = strtolower($string); // make it lowercase
-   
+
       preg_match_all('/\b.*?\b/i', $string, $matchWords);
       $matchWords = $matchWords[0];
-      
+
       foreach ( $matchWords as $key=>$item ) {
           if ( $item == '' || in_array(strtolower($item), $stopWords) || strlen($item) <= 3 ) {
               unset($matchWords[$key]);
           }
-      }   
+      }
       $wordCountArr = array();
       if ( is_array($matchWords) ) {
           foreach ( $matchWords as $key => $val ) {
@@ -221,9 +224,6 @@ function extractCommonWords($string){
       $wordCountArr = array_slice($wordCountArr, 0, 10);
       return $wordCountArr;
 }
-
-define('THUMBNAIL_IMAGE_MAX_WIDTH', 320);
-define('THUMBNAIL_IMAGE_MAX_HEIGHT', 640);
 
 function generateThumbnail($source_image_path, $thumbnail_image_path)
 {
@@ -410,11 +410,11 @@ function bsAlert ( $type, $message )
         case 'error':
             $message = '<div class="alert alert-danger">' . $message . '</div>';
             break;
-	   
+
 	   case 'info':
 	   	  $message = '<div class="alert alert-info">' . $message . '</div>';
 	   	  break;
-	   	  
+
 	   case 'warning':
 	       $message = '<div class="alert alert-warning">' . $message . '</div>';
 	   	  break;
@@ -471,8 +471,6 @@ function makeClickableLinks($text)
   return $text;
 }
 
-/* Helper functions: */
-
 function get_tag($tag,$xml)
 {
 	preg_match_all('/<'.$tag.'>(.*)<\/'.$tag.'>$/imU',$xml,$match);
@@ -482,7 +480,7 @@ function get_tag($tag,$xml)
 function is_bot()
 {
 	/* This function will check whether the visitor is a search engine robot */
-	
+
 	$botlist = array("Teoma", "alexa", "froogle", "Gigabot", "inktomi",
 	"looksmart", "URL_Spider_SQL", "Firefly", "NationalDirectory",
 	"Ask Jeeves", "TECNOSEEK", "InfoSeek", "WebFindBot", "girafabot",
