f84cc6fc2ac241d67d7e218c460931f942fc37b8
mbp2011 <webmaster@mbp2011.org>
2015-02-15 01:07:10 -0500

    cleanup file structure

--- lib/autoload.php
+++ lib/autoload.php
@@ -7,11 +7,10 @@ $library = array
         'class.library.php',                      //Custom PHP Functions
         'class.parsedown.php',                    //Markdown Parsing
         'phpMailer/PHPMailerAutoload.php',        //phpMailer Mail Transport Class
-        'class.login.php',
-        'class.registration.php',
-        'class.turncate.php',
-        'class.rsslib.php',
-        'class.S3.php'
+        'class.login.php',                        //PHP-LOGIN Project Login Class
+        'class.registration.php',                 //PHP-LOGIN Project Regisistration Class
+        'class.turncate.php',                     //CAKE-PHP text turncate function
+        'class.S3.php'                            //Amazon AWS S3 Class
 );
 
 foreach ($library AS $class)
