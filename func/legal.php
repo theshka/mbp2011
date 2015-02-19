d63d8d7c6a6d1ac83cce148ee58c5bbe1c10c51e
mbp2011 <webmaster@mbp2011.org>
2015-02-18 19:18:01 -0500

    update privacy policy

--- func/legal.php
+++ func/legal.php
@@ -12,27 +12,27 @@
 				<h3>
 					1. Terms
 				</h3>
-				
+
 				<p>
-					By accessing this web site, you are agreeing to be bound by these 
-					web site Terms and Conditions of Use, all applicable laws and regulations, 
-					and agree that you are responsible for compliance with any applicable local 
-					laws. If you do not agree with any of these terms, you are prohibited from 
-					using or accessing this site. The materials contained in this web site are 
+					By accessing this web site, you are agreeing to be bound by these
+					web site Terms and Conditions of Use, all applicable laws and regulations,
+					and agree that you are responsible for compliance with any applicable local
+					laws. If you do not agree with any of these terms, you are prohibited from
+					using or accessing this site. The materials contained in this web site are
 					protected by applicable copyright and trade mark law.
 				</p>
-				
+
 				<h3>
 					2. Use License
 				</h3>
-				
+
 				<ol type="a">
 					<li>
-						Permission is granted to temporarily download one copy of the materials 
-						(information or software) on #MBP2011's web site for personal, 
-						non-commercial transitory viewing only. This is the grant of a license, 
+						Permission is granted to temporarily download one copy of the materials
+						(information or software) on #MBP2011's web site for personal,
+						non-commercial transitory viewing only. This is the grant of a license,
 						not a transfer of title, and under this license you may not:
-						
+
 						<ol type="i">
 							<li>modify or copy the materials;</li>
 							<li>use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</li>
@@ -45,53 +45,53 @@
 						This license shall automatically terminate if you violate any of these restrictions and may be terminated by #MBP2011 at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.
 					</li>
 				</ol>
-				
+
 				<h3>
 					3. Disclaimer
 				</h3>
-				
+
 				<ol type="a">
 					<li>
 						The materials on #MBP2011's web site are provided "as is". #MBP2011 makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights. Further, #MBP2011 does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its Internet web site or otherwise relating to such materials or on any sites linked to this site.
 					</li>
 				</ol>
-				
+
 				<h3>
 					4. Limitations
 				</h3>
-				
+
 				<p>
 					In no event shall #MBP2011 or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption,) arising out of the use or inability to use the materials on #MBP2011's Internet site, even if #MBP2011 or a #MBP2011 authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
 				</p>
-							
+
 				<h3>
 					5. Revisions and Errata
 				</h3>
-				
+
 				<p>
 					The materials appearing on #MBP2011's web site could include technical, typographical, or photographic errors. #MBP2011 does not warrant that any of the materials on its web site are accurate, complete, or current. #MBP2011 may make changes to the materials contained on its web site at any time without notice. #MBP2011 does not, however, make any commitment to update the materials.
 				</p>
-				
+
 				<h3>
 					6. Links
 				</h3>
-				
+
 				<p>
 					#MBP2011 has not reviewed all of the sites linked to its Internet web site and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by #MBP2011 of the site. Use of any such linked web site is at the user's own risk.
 				</p>
-				
+
 				<h3>
 					7. Site Terms of Use Modifications
 				</h3>
-				
+
 				<p>
 					#MBP2011 may revise these terms of use for its web site at any time without notice. By using this web site you are agreeing to be bound by the then current version of these Terms and Conditions of Use.
 				</p>
-				
+
 				<h3>
 					8. Governing Law
 				</h3>
-				
+
 				<p>
 					Any claim relating to #MBP2011's web site shall be governed by the laws of the Country of Canada without regard to its conflict of law provisions.
 				</p>
@@ -114,24 +114,26 @@
 			</div>
 
 			<div class="modal-body overflowing" style="height: 600px;">
-				This Privacy Policy governs the manner in which #MBP2011 collects, uses, maintains and discloses information collected from users (each, a "User") of the <a href="http://mbp2011.org">http://mbp2011.org</a> website ("Site"). This privacy policy applies to the Site and all products and services offered by #MBP2011.<br><br>
+				This Privacy Policy governs the manner in which #MBP2011 collects, uses, maintains and discloses information collected from users (each, a "User") of the <a href="https://mbp2011.org">https://mbp2011.org</a> website ("Site"). This privacy policy applies to the Site and all products and services offered by #MBP2011.<br><br>
 
 				<b>Personal identification information</b><br><br>
-				
-				We may collect personal identification information from Users in a variety of ways, including, but not limited to, when Users visit our site, fill out a form, and in connection with other activities, services, features or resources we make available on our Site. Users may be asked for, as appropriate, name, email address. Users may, however, visit our Site anonymously. We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.<br><br>
-				
+
+				We may collect personal identification information from Users in a variety of ways, including, but not limited to, when Users visit our site, register on the site, subscribe to the newsletter, respond to a survey, fill out a form, and in connection with other activities, services, features or resources we make available on our Site. Users may be asked for, as appropriate, name, email address. Users may, however, visit our Site anonymously. We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.<br><br>
+
 				<b>Non-personal identification information</b><br><br>
-				
+
 				We may collect non-personal identification information about Users whenever they interact with our Site. Non-personal identification information may include the browser name, the type of computer and technical information about Users means of connection to our Site, such as the operating system and the Internet service providers utilized and other similar information.<br><br>
-				
+
 				<b>Web browser cookies</b><br><br>
-				
+
 				Our Site may use "cookies" to enhance User experience. User's web browser places cookies on their hard drive for record-keeping purposes and sometimes to track information about them. User may choose to set their web browser to refuse cookies, or to alert you when cookies are being sent. If they do so, note that some parts of the Site may not function properly.<br><br>
-				
+
 				<b>How we use collected information</b><br><br>
-				
+
 				#MBP2011 may collect and use Users personal information for the following purposes:<br>
 				<ul>
+				<li><i>- To improve customer service</i><br>
+					Information you provide helps us respond to your customer service requests and support needs more efficiently.</li>
 				<li><i>- To personalize user experience</i><br>
 					We may use information in the aggregate to understand how our Users as a group use the services and resources provided on our Site.</li>
 				<li><i>- To improve our Site</i><br>
@@ -139,37 +141,39 @@
 				<li><i>- To run a promotion, contest, survey or other Site feature</i><br>
 					To send Users information they agreed to receive about topics we think will be of interest to them.</li>
 				<li><i>- To send periodic emails</i><br>
-				We may use the email address to respond to their inquiries, questions, and/or other requests. If User decides to opt-in to our mailing list, they will receive emails that may include company news, updates, related product or service information, etc. If at any time the User would like to unsubscribe from receiving future emails, we include detailed unsubscribe instructions at the bottom of each email.</li>
+				We may use the email address to send User information and updates pertaining to their order. It may also be used to respond to their inquiries, questions, and/or other requests. If User decides to opt-in to our mailing list, they will receive emails that may include company news, updates, related product or service information, etc. If at any time the User would like to unsubscribe from receiving future emails, we include detailed unsubscribe instructions at the bottom of each email or User may contact us via our Site.</li>
 				</ul>
 				<b>How we protect your information</b><br><br>
-				
+
 				We adopt appropriate data collection, storage and processing practices and security measures to protect against unauthorized access, alteration, disclosure or destruction of your personal information, username, password, transaction information and data stored on our Site.<br><br>
-				
+
+				Sensitive and private data exchange between the Site and its Users happens over a SSL secured communication channel and is encrypted and protected with digital signatures.<br><br>
+
 				<b>Sharing your personal information</b><br><br>
-				
-				We do not sell, trade, or rent Users personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding visitors and users with our business partners, trusted affiliates and advertisers for the purposes outlined above. You may also view this aggregate data at the following link: <a href="/?page=analytics">http://mbp2011.org/?page=analytics</a><br><br>
-				
+
+				We do not sell, trade, or rent Users personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding visitors and users with our business partners, trusted affiliates and advertisers for the purposes outlined above.We may use third party service providers to help us operate our business and the Site or administer activities on our behalf, such as sending out newsletters or surveys. We may share your information with these third parties for those limited purposes provided that you have given us your permission.<br><br>
+
 				<b>Third party websites</b><br><br>
-				
+
 				Users may find advertising or other content on our Site that link to the sites and services of our partners, suppliers, advertisers, sponsors, licensors and other third parties. We do not control the content or links that appear on these sites and are not responsible for the practices employed by websites linked to or from our Site. In addition, these sites or services, including their content and links, may be constantly changing. These sites and services may have their own privacy policies and customer service policies. Browsing and interaction on any other website, including websites which have a link to our Site, is subject to that website's own terms and policies.<br><br>
-				
+
+				<b>Advertising</b><br><br>
+
+				Ads appearing on our site may be delivered to Users by advertising partners, who may set cookies. These cookies allow the ad server to recognize your computer each time they send you an online advertisement to compile non personal identification information about you or others who use your computer. This information allows ad networks to, among other things, deliver targeted advertisements that they believe will be of most interest to you. This privacy policy does not cover the use of cookies by any advertisers.<br><br>
+
 				<b>Changes to this privacy policy</b><br><br>
-				
+
 				#MBP2011 has the discretion to update this privacy policy at any time. When we do, we will revise the updated date at the bottom of this page. We encourage Users to frequently check this page for any changes to stay informed about how we are helping to protect the personal information we collect. You acknowledge and agree that it is your responsibility to review this privacy policy periodically and become aware of modifications.<br><br>
-				
+
 				<b>Your acceptance of these terms</b><br><br>
-				
-				By using this Site, you signify your acceptance of this policy. If you do not agree to this policy, please do not use our Site. Your continued use of the Site following the posting of changes to this policy will be deemed your acceptance of those changes.<br><br>
-				
+
+				By using this Site, you signify your acceptance of this policy and <a href="https://mbp2011.org/?func=legal">terms of service</a>. If you do not agree to this policy, please do not use our Site. Your continued use of the Site following the posting of changes to this policy will be deemed your acceptance of those changes.<br><br>
+
 				<b>Contacting us</b><br><br>
-				
-				If you have any questions about this Privacy Policy, the practices of this site, or your dealings with this site, please contact us at:<br>
-				
-				<br>
-				
+
 				<address>
 					<strong>#MBP2011 Community</strong><br>
-					Omnipresent<br>
+					Omnipresent, Worldwide<br>
 					<abbr title="Email">E:</abbr> webmaster@mbp2011.org<br>
 					<a href="http://mbp2011.org">http://mbp2011.org</a>
 				</address>
@@ -177,9 +181,8 @@
 				<br>
 				<p>
 					<b><?php echo "Last modified: " . date ("F d, Y", getlastmod()); ?></b>
-				</p>	
+				</p>
 			</div>
 		</div>
 	</div>
 </div>
-
